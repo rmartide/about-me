09e0308721c461e795e9f65c18adcd022907c581
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-08-01 20:00:16 +0200

    Added components for all the sections

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { BiographyComponent } from './biography/biography.component';
+import { StackComponent } from './stack/stack.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, SkillsComponent],
+  declarations: [HomeComponent, SkillsComponent, BiographyComponent, StackComponent, CvComponent, ProjectsComponent, HobbiesComponent, ContactComponent],
   imports: [
     CommonModule
   ]
