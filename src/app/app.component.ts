2245bb0a5c7271b68b89246540f3cc0fc10336f5
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-08-11 20:49:18 +0200

    Added example of mat lists for the sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'about-me';
+  folders = [
+    {name: 'folder1', updated: 'Update yesterday'},
+    {name: 'folder2', updated: 'Update 2018'},
+    {name: 'folder3', updated: 'Update 3 days ago'},
+  ]
+
+  notes = [
+    {name: 'note1', updated: 'Update yesterday'},
+    {name: 'note2', updated: 'Update 2018'},
+    {name: 'note3', updated: 'Update 3 days ago'},
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { ViewsModule } from './views';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ViewsModule } from "./views";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SectionStepperComponent } from './navigation/section-stepper/section-stepper.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SectionStepperComponent } from "./navigation/section-stepper/section-stepper.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { SidenavComponent } from "./navigation/sidenav/sidenav.component";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SectionStepperComponent,
-    SidenavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ViewsModule,
-    BrowserAnimationsModule,
-    MatSidenavModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, SectionStepperComponent, SidenavComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ViewsModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+    MatDividerModule,
+    MatListModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
