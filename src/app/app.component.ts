29cd36621b4457c9987419ce695c325ef4474834
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-08-12 20:56:12 +0200

    Adding sections array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,16 @@ export class AppComponent {
     {name: 'note2', updated: 'Update 2018'},
     {name: 'note3', updated: 'Update 3 days ago'},
   ]
+
+  // This will need to come from i18e
+  sections = [
+    { id: 'home', display: 'Home'},
+    { id: 'bio', display: 'Biography'},
+    { id: 'skills', display: 'Skills'},
+    { id: 'stack', display: 'Stack'},
+    { id: 'projects', display: 'Projects'},
+    { id: 'hobbies', display: 'Hobbies'},
+    { id: 'cv', display: 'Curriculum Vitae'},
+    { id: 'contact', display: 'Contact'},
+  ]
 }
